e3beebe92b3bf8eb59b724da68dd62ddf4b49847
dominik.roemer <dominik.roemer@accenture.com>
2025-06-22 09:34:14 +0200

    server scripts debug

--- server_scripts/execute_daily_pipelines.py
+++ server_scripts/execute_daily_pipelines.py
@@ -1,4 +1,4 @@
-from server_scripts.fetch_new_posts_from_subreddit import pipeline as new_posts_pipeline
+from .fetch_new_posts_from_subreddit import pipeline as new_posts_pipeline
 
 if __name__ == "__main__":
     new_posts_pipeline()
--- server_scripts/execute_weekly_pipelines.py
+++ server_scripts/execute_weekly_pipelines.py
@@ -1,4 +1,4 @@
-from server_scripts.fetch_weekly_cc_prices import pipeline as weekly_prices_pipeline
+from .fetch_weekly_cc_prices import pipeline as weekly_prices_pipeline
 
 if __name__ == "__main__":
     print("Executing daily pipeline from top-level entry point...")
